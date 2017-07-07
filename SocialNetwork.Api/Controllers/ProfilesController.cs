a913ee1ed922b37454e83d0a73def6de8b12b98e
Evan Aktoudianakis <Evangelos.Aktoudianakis@zopa.com>
2017-07-07 16:37:45 +0100

    First commit

---
+++ SocialNetwork.Api/Controllers/ProfilesController.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using SocialNetwork.Api.Helpers;
+using SocialNetwork.Data.Models;
+using SocialNetwork.Data.Repositories;
+
+namespace SocialNetwork.Api.Controllers
+{
+    using System.Security.Claims;
+
+    public class ProfilesController : SocialNetworkApiController
+    {
+        private readonly IProfileRepository profileRepository;
+        private readonly IUserRepository userRepository;
+
+        public ProfilesController(IProfileRepository profileRepository, IUserRepository userRepository)
+        {
+            this.profileRepository = profileRepository;
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IHttpActionResult> GetAsync()
+        {
+            var claimsPrincipal = User as ClaimsPrincipal;
+
+            var user = await userRepository.GetAsync(claimsPrincipal.FindFirst("").Value);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var profile = await profileRepository.GetForAsync(user);
+
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(profile);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> PutAsync(string username, string password, [FromBody]Profile profile)
+        {
+            var user = await userRepository.GetAsync(username, HashHelper.Sha512(password + username));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var toUpdate = await profileRepository.GetForAsync(user);
+
+            if (toUpdate == null)
+            {
+                return NotFound();
+            }
+
+            toUpdate.Bio = profile.Bio;
+
+            await profileRepository.UpdateAsync(toUpdate);
+
+            return Ok(toUpdate);
+        }
+    }
+}
---
+++ SocialNetwork.OAuth/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialNetwork.OAuth.Startup))]
+
+namespace SocialNetwork.OAuth
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Security.Claims;
+    using System.Security.Cryptography.X509Certificates;
+    using IdentityServer3.Core;
+    using IdentityServer3.Core.Configuration;
+    using IdentityServer3.Core.Models;
+    using IdentityServer3.Core.Services.InMemory;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(InMemoryManager.GetUsers())
+                .UseInMemoryScopes(InMemoryManager.GetScopes())
+                .UseInMemoryClients(InMemoryManager.GetClients());
+                
+            var certificate = Convert.FromBase64String(ConfigurationManager.AppSettings["SigningCertificate"]);
+
+            var options = new IdentityServerOptions()
+            {
+                SigningCertificate = new X509Certificate2(certificate, ConfigurationManager.AppSettings["SigningCertificatePassword"]),
+                RequireSsl = false,
+                Factory = factory
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+
+    public static class InMemoryManager
+    {
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>()
+            {
+                new InMemoryUser()
+                {
+                    Subject = "eaktoudianakis@gmail.com",
+                    Username = "eaktoudianakis@gmail.com",
+                    Password = "password",
+                    Claims = new List<Claim>()
+                    {
+                        new Claim(Constants.ClaimTypes.Name, "Evan Aktoudianakis")
+                    }
+                }
+            };   
+        }
+
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new[]
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                StandardScopes.OfflineAccess,
+                new Scope
+                {
+                    Name = "read",
+                    DisplayName = "Read User Data"
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client()
+                {
+                    ClientId = "socialnetwork",
+                    ClientSecrets = new List<Secret>()
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    Flow = Flows.ResourceOwner,
+                    AllowedScopes = new List<string>()
+                    {
+                        Constants.StandardScopes.OpenId,
+                        "read",
+                        Constants.StandardScopes.Profile
+                    },
+                    Enabled = true
+                }
+            };
+        }
+    }
+}
